3b7d0606623c86fcc683e6eb2592a74956f9169b
Mberumen4 <mberumen2005@gmail.com>
2025-04-13 10:40:44 -0400

    Add files via upload
    add to git hub

---
+++ ReverseWithStack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ReverseWithStack
+{
+    public static void Run()
+    {
+        Console.Write("Enter number of integers: ");
+        int n = int.Parse(Console.ReadLine());
+        Stack<int> numbers = new Stack<int>();
+
+        Console.WriteLine("Enter integers:");
+        for (int i = 0; i < n; i++)
+        {
+            numbers.Push(int.Parse(Console.ReadLine()));
+        }
+
+        Console.WriteLine("Reversed order:");
+        while (numbers.Count > 0)
+        {
+            Console.WriteLine(numbers.Pop());
+        }
+    }
+}
